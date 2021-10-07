4523379f8791efc02e46916955b19e9386e21592
Fangjun Kuang <csukuangfj@gmail.com>
2021-10-07 08:55:18 +0800

    Fix installation on macOS. (#13)

--- setup.py
+++ setup.py
@@ -36,6 +36,13 @@ class BuildExtension(build_ext):
             )
         lib_so = glob.glob(f'{build_dir}/lib/*.so*')
         for so in lib_so:
+            print(f'Copying {so} to {self.build_lib}/')
+            shutil.copy(f'{so}', f'{self.build_lib}/')
+
+        # macos
+        lib_so = glob.glob(f'{build_dir}/lib/*kaldilm*.dylib')
+        for so in lib_so:
+            print(f'Copying {so} to {self.build_lib}/')
             shutil.copy(f'{so}', f'{self.build_lib}/')
 
 
