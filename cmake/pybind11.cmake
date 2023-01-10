9f754769fff12a07de14a488fa147cc47164402c
Fangjun Kuang <csukuangfj@gmail.com>
2023-01-10 13:54:52 +0800

    Publish pre-built wheels to PyPI (#27)

--- cmake/pybind11.cmake
+++ cmake/pybind11.cmake
@@ -1,5 +1,3 @@
-# Copyright (c)  2020  Xiaomi Corporation (author: Fangjun Kuang)
-
 function(download_pybind11)
   if(CMAKE_VERSION VERSION_LESS 3.11)
     list(APPEND CMAKE_MODULE_PATH ${CMAKE_SOURCE_DIR}/cmake/Modules)
@@ -7,21 +5,27 @@ function(download_pybind11)
 
   include(FetchContent)
 
-  set(pybind11_URL  "https://github.com/pybind/pybind11/archive/v2.6.0.tar.gz")
-  set(pybind11_HASH "SHA256=90b705137b69ee3b5fc655eaca66d0dc9862ea1759226f7ccd3098425ae69571")
+  set(pybind11_URL  "https://github.com/pybind/pybind11/archive/refs/tags/v2.10.2.tar.gz")
+  set(pybind11_HASH "SHA256=93bd1e625e43e03028a3ea7389bba5d3f9f2596abc074b068e70f4ef9b1314ae")
 
-  set(double_quotes "\"")
-  set(dollar "\$")
-  set(semicolon "\;")
+  # If you don't have access to the Internet, please download it to your
+  # local drive and modify the following line according to your needs.
+  if(EXISTS "/star-fj/fangjun/download/github/pybind11-2.10.2.tar.gz")
+    set(pybind11_URL  "file:///star-fj/fangjun/download/github/pybind11-2.10.2.tar.gz")
+  elseif(EXISTS "/Users/fangjun/Downloads/pybind11-2.10.2.tar.gz")
+    set(pybind11_URL  "file:///Users/fangjun/Downloads/pybind11-2.10.2.tar.gz")
+  elseif(EXISTS "/tmp/pybind11-2.10.2.tar.gz")
+    set(pybind11_URL  "file:///tmp/pybind11-2.10.2.tar.gz")
+  endif()
+
   FetchContent_Declare(pybind11
     URL               ${pybind11_URL}
     URL_HASH          ${pybind11_HASH}
-    PATCH_COMMAND
   )
 
   FetchContent_GetProperties(pybind11)
   if(NOT pybind11_POPULATED)
-    message(STATUS "Downloading pybind11")
+    message(STATUS "Downloading pybind11 from ${pybind11_URL}")
     FetchContent_Populate(pybind11)
   endif()
   message(STATUS "pybind11 is downloaded to ${pybind11_SOURCE_DIR}")
@@ -29,3 +33,4 @@ function(download_pybind11)
 endfunction()
 
 download_pybind11()
+
